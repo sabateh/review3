22a980c5f50bc8b26d566bf82f94be26c8255de4
sabateh <saba70.tehrani@gmail.com>
2023-01-12 20:30:28 -0500

    pushing code from review 3

---
+++ src/review1.java
@@ -0,0 +1,11 @@
+public class review1 {
+    public static void main(String[] args) {
+        boolean loginButtonDisplayed=true;
+        boolean loginButtonClickable=false;
+        if(loginButtonClickable&&loginButtonDisplayed){
+            System.out.println("test case past");
+        }else{
+            System.out.println("test case failed");
+        }
+    }
+}
